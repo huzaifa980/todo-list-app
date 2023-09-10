349645517fdeef0048851a49ba61036eaf4e7083
Muhammad Huzaifa <136707896+huzaifa980@users.noreply.github.com>
2023-09-10 23:40:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+document.querySelector('#push').onclick = 
+function() {
+if(document.querySelector ('#newtask input').value.length == 0){
+    alert("please Enter a task");
+}
+else{
+    document.querySelector('#tasks').innerHTML
+    += `<div class="task"><span id="taskname">${document.querySelector ("#newtask input").value}</span><button class="delete">X</button> </div>` ;
+
+    
+var current_tasks = document.
+querySelectorAll('.delete');
+
+for (var i = 0; i < current_tasks.length; i++) {
+    current_tasks[i].onclick = function () {
+         this.parentNode.remove();
+
+    }
+        
+    }
+
+
+    
+
+        document.querySelector('#newtask input').value ="";
+    
+   
+    }
+    
+}
+    
+
+
+
+
